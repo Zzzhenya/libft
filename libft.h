c3bc90123c53a16487e832af3d6297bb008390ef
Shenya De Silva <shenya44@outlook.com>
2023-05-12 23:15:55 +0200

    Initial commit libft.h

---
+++ libft.h
@@ -0,0 +1,23 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+int	ft_tolower(int c);
+int	ft_isalpha(int c);
+int	ft_toupper(int c);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	*ft_memset(void *s, int c, size_t n);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putstr_fd(char *s, int fd);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+
+#endif
