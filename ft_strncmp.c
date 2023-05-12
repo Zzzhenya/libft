07fa4953e51e4ff7a5c11383dd61b2a948d8e2d7
Shenya De Silva <sde-silv@c4a8c10.42berlin.de>
2023-05-12 17:09:33 +0200

    Updated

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: sde-silv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 12:29:51 by sde-silv          #+#    #+#             */
-/*   Updated: 2023/05/12 13:41:23 by sde-silv         ###   ########.fr       */
+/*   Updated: 2023/05/12 17:07:57 by sde-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,16 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (s1[i] == s2[i] && i < n)
-		i ++;
-	return (s1[i] - s2[i]);
+	while ((s1[i] || s2[i]) && i < n )
+	{
+
+		if (s1[i] == s2[i])
+			i ++;
+		else
+			return (((unsigned char*)s1)[i] - ((unsigned char*)s2)[i]);
+
+	}
+	return (0);
 }
 
 int	main(int argc, char **argv)
