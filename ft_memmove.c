09a3f2d36311a2df9eaa3c50ff617009b3c139f0
Shenya De Silva <sde-silv@c4a9c9.42berlin.de>
2023-05-24 15:09:08 +0200

    Updated memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -3,58 +3,58 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sde-silv <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: sde-silv <sde-silv@student@42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 13:58:56 by sde-silv          #+#    #+#             */
-/*   Updated: 2023/05/22 14:11:01 by sde-silv         ###   ########.fr       */
+/*   Updated: 2023/05/24 15:07:29 by sde-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "libft.h"
-
+#include "libft.h"
+/*
 #include <string.h>
 #include <unistd.h>
-
+*/
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	char	*char_src;
-	char	*char_dest;
-	char	temp[n];
+	char	*char_s;
+	char	*char_d;
 	size_t	i;
 
 	i = 0;
-	char_src = (char *)src;
-	char_dest = (char *)dest;
+	char_s = (char *)src;
+	char_d = (char *)dest;
 	while (i < n)
 	{
-		temp[i] = char_src[i];
-		i ++;
-	}
-	i = 0;
-	while (i < n)
-	{
-		char_dest[i] = temp[i];
-		i ++;
+		if (dest < src)
+		{
+			char_d[i] = char_s[i];
+			i ++;
+		}
+		else
+		{
+			char_d[n - 1] = char_s[n - 1];
+			n --;
+		}
 	}
 	return (dest);
 }
-
+/*
 int	main(void)
 {
+	char	dest[12];
 	char    *src = "Hello World!";
-	char    dest[12];
 	char    *rtn;
 	int     n;
 
 	n = 5;
 
 	write (1,src, 12);
-	write (1, "\n", 1);
-	write (1, dest, 12);
 	write(1, "\n", 1);
-	rtn = ft_memmove(dest, src, n);
+	rtn = memmove(dest, src, n);
 	write(1,rtn, n);
 	write (1, "\n", 1);
 	return (0);
 }
+*/
