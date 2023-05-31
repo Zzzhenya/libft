b36d5749bcce35c2d76c1322e11f172f7d6232d4
Shenya De Silva <sde-silv@c4a9c8.42berlin.de>
2023-05-31 13:01:22 +0200

    Updated prototype error fix

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: sde-silv <sde-silv@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 15:13:54 by sde-silv          #+#    #+#             */
-/*   Updated: 2023/05/31 12:52:08 by sde-silv         ###   ########.fr       */
+/*   Updated: 2023/05/31 12:57:21 by sde-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
 char	**ft_split(char const *s, char c);
 
-void	*ft_memset(void *s, int cm size_t n);
+void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
