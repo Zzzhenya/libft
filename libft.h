2e879584fb33b4f6a6af065ab70773f414753d22
Shenya De Silva <sde-silv@c4a8c10.42berlin.de>
2023-06-01 13:48:10 +0200

    Updated libft.h

--- libft.h
+++ libft.h
@@ -6,12 +6,12 @@
 /*   By: sde-silv <sde-silv@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 15:13:54 by sde-silv          #+#    #+#             */
-/*   Updated: 2023/06/01 13:46:23 by sde-silv         ###   ########.fr       */
+/*   Updated: 2023/06/01 13:47:56 by sde-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef __LIBFT_H__
-# define __LIBFT_H__
+#ifndef LIBFT_H
+# define LIBFT_H
 
 # include <unistd.h>
 
