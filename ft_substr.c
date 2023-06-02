62d26dba9de29f74658fc29ad311f60f3416f850
Shenya De Silva <sde-silv@c4a9c10.42berlin.de>
2023-06-02 18:28:33 +0200

    Updated length

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: sde-silv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 13:52:02 by sde-silv          #+#    #+#             */
-/*   Updated: 2023/06/02 18:04:18 by sde-silv         ###   ########.fr       */
+/*   Updated: 2023/06/02 18:28:11 by sde-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,23 +21,21 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	i = 0;
 	ptr = NULL;
-	if (start <= ft_strlen(s))
-		while (*s && start > 0)
-			start --;
-		if (len - 1 < ft_strlen(s + (char)start))
+	if (s && start <= ft_strlen(s))
+		if (len < ft_strlen(s + start))
 		{
-			ptr = malloc(len)
-			while (s[start + i] && i < len)
+			ptr = malloc(len + 1);
+			while (s[start + i] && i <= len)
 			{
 				ptr[i] = s[start + i];
 				i ++;
 			}
-			ptr[i] = '\0';
+			ptr[i] = '\0';		
 		}
 		else
 		{
-			ptr = malloc(ft_strlen(s + (char)start) + 1)
-			while (s[start + i] && i < ft_strlen(s + (char)start))
+			ptr = malloc(ft_strlen(s + start) + 1);
+			while (s[start + i] && i < ft_strlen(s + start))
 			{
 				ptr[i] = s[start + i];
 				i ++;
