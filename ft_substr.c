5bfa7734fc9a7ded5b466445f465b32916992071
Shenya De Silva <sde-silv@c4a9c10.42berlin.de>
2023-06-02 17:30:57 +0200

    Logic change

--- ft_substr.c
+++ ft_substr.c
@@ -6,40 +6,25 @@
 /*   By: sde-silv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 13:52:02 by sde-silv          #+#    #+#             */
-/*   Updated: 2023/06/02 15:53:00 by sde-silv         ###   ########.fr       */
+/*   Updated: 2023/06/02 17:29:22 by sde-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
+
 /*
 #include <unistd.h>
 */
-
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	unsigned int	i;
-	char			*ptr;
+	char	*ptr;
 
-	i = 0;
-	while (s[i] && i < start)
-		i ++;
-	if (s[i] && i == start)
-	{
-		if (len <= ft_strlen(s + i) - 1)
-			ptr = malloc(len);
-		else
-			ptr = malloc(ft_strlen(s + i));
-		i = 0;
-		while (s[start + i] && i + 1 < len)
-		{
-			ptr[i] = s[start + i];
-			i ++;
-		}
-		ptr[i] = '\0';
+	if (start <= ft_strlen(s) - 1)
+		ptr = malloc(len);
+		ft_strlcpy(ptr, s + start, len);
 		return (ptr);
-	}
-	return (NULL);
+	return (NULL);	
 }
 /*
 int	main(void)
@@ -48,11 +33,12 @@ int	main(void)
 	char	*ptr;
 	size_t	len;
 
-	len = 4;
-	str = "Abcdefghij";
-	ptr = ft_substr(str, 3, len);
+	len = 1;
+	str = "";
+	ptr = ft_substr(str, 0, len);
 	ft_putstr_fd(ptr, 1);
 	write (1, "\n", 1);
+	free(ptr);
 	return (0);
 }
 */
