22ee983f08f0a13929b7ef64e932f3e6edd99da9
Shenya De Silva <sde-silv@c4a9c10.42berlin.de>
2023-06-02 18:46:16 +0200

    Reverting to pseudocode fot ft_substr)

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: sde-silv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 13:52:02 by sde-silv          #+#    #+#             */
-/*   Updated: 2023/06/02 18:28:11 by sde-silv         ###   ########.fr       */
+/*   Updated: 2023/06/02 17:43:25 by sde-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,31 +16,18 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*ptr;
-	size_t	i;
-
-	i = 0;
-	ptr = NULL;
-	if (s && start <= ft_strlen(s))
-		if (len < ft_strlen(s + start))
-		{
-			ptr = malloc(len + 1);
-			while (s[start + i] && i <= len)
-			{
-				ptr[i] = s[start + i];
-				i ++;
-			}
-			ptr[i] = '\0';		
-		}
+	initiate pointer to NULL
+	if index/start is in s
+	go to index/start
+	check the size of the substring
+	compare it with len
+		if len - 1 < string after start
+			create memory area for size len
+			copy until len - 1
+			add null terminator
 		else
-		{
-			ptr = malloc(ft_strlen(s + start) + 1);
-			while (s[start + i] && i < ft_strlen(s + start))
-			{
-				ptr[i] = s[start + i];
-				i ++;
-			}
-			ptr[i] = '\0';
-		}
-	return (ptr);
+			create memory area for size + 1
+			copy until size
+			add null terminator
+	return pointer to allocated substring memory area
 }
