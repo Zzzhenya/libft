ad33f56ba6bfda92e734ac99aaf2bed51e8d7b61
Shenya De Silva <shenya44@outlook.com>
2023-06-06 17:39:26 +0200

    revert back to pointer arithmetic

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -24,18 +24,16 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t	end;
 	char	*ptr;
 
-	if (s1 == NULL || set == NULL)
-		return (NULL);
+	ptr = 0;
 	start = 0;
+	end = ft_strlen(s1) - 1;
 	while (s1[start] && ft_strchr(set, s1[start]))
 		start ++;
-	end = ft_strlen(s1) - 1;
 	while (end + 1 > start && ft_strchr(set, s1[end]))
 		end --;
 	ptr = (char *)malloc(sizeof(char) * (end - start + 2));
-	if (ptr == NULL)
-		return (NULL);
-	ft_strlcpy(ptr, &s1[start], (end - start + 2));
+	if (ptr)
+		ft_strlcpy(ptr, (s1 + start), (end - start + 2));
 	return (ptr);
 }
 /*
