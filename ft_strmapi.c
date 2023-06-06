8fcac071e469645594fcadb41d2a365f60be9d24
Shenya De Silva <sde-silv@c4a8c10.42berlin.de>
2023-06-06 19:38:25 +0200

    Initial commit strmapi.c

---
+++ ft_strmapi.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sde-silv <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/06 18:08:35 by sde-silv          #+#    #+#             */
+/*   Updated: 2023/06/06 19:37:33 by sde-silv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+/*
+#include <unistd.h>
+
+char ft_toupper_fd(unsigned int i, char c)
+{
+    if ('a' <= (int)c && (int)c <= 'z')
+        return ((char)((int)c - 32));
+    else
+        return (c);
+}
+*/
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char	*c;
+
+	c = malloc(ft_strlen(s) + 1);
+	if (!c)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		c[i] = (*f)(i, s[i]);
+		i ++;
+	}
+	return(c);
+}
+/*
+int	main(void)
+{
+	char	*ptr;
+	ptr = "Hello\n";
+	write(1,ft_strmapi(ptr, ft_toupper_fd),6);
+	return (0);
+}
+*/
