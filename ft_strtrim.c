800e7e643f6bfd20e1520e3d1dcde79416042511
Shenya De Silva <sde-silv@c4a8c10.42berlin.de>
2023-06-06 15:18:32 +0200

    Updated strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,12 +6,14 @@
 /*   By: sde-silv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 10:59:37 by sde-silv          #+#    #+#             */
-/*   Updated: 2023/06/06 14:56:27 by sde-silv         ###   ########.fr       */
+/*   Updated: 2023/06/06 15:17:50 by sde-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-#include <unistd.h>
+char	*ft_strchr(const char *s, int c)
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+
 */
 #include <stdlib.h>
 #include "libft.h"
@@ -22,19 +24,16 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t	end;
 	char	*ptr;
 
-	ptr = 0;
 	start = 0;
+	ptr = 0;
 	end = ft_strlen(s1);
 	while (s1[start] && ft_strchr(set, s1[start]))
-			start ++;
+		start ++;
 	while (s1[end] && ft_strchr(set, s1[end]))
 		end --;
-	ptr = malloc(sizeof(char) * (end - start + 1));
+	ptr = malloc (sizeof(char) * (end - start + 1));
 	if (ptr)
-	{
-		ft_strlcpy(ptr, (s1 + start), (end - start));
-		ft_strlcpy(ptr + start + end, "\0", 1);
-	}
+		ft_strlcpy(ptr, s1 + start, (end - start));
 	return (ptr);
 }
 /*
@@ -42,8 +41,7 @@ int	main(void)
 {
 	char *ptr;
 
-	write(1, "Hello\n", 6);
-	ptr = ft_strtrim(" Hello ", "Hel ");
+	ptr = ft_strtrim("  ", " ");
 	write(1, ptr, ft_strlen(ptr));
 	free (ptr);
 	return (0);
