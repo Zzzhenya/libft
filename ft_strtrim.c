6231923f1544fac79f24579fac9e6be8cfca06f6
Shenya De Silva <sde-silv@c4a8c10.42berlin.de>
2023-06-06 15:39:46 +0200

    Error correction

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: sde-silv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 10:59:37 by sde-silv          #+#    #+#             */
-/*   Updated: 2023/06/06 15:17:50 by sde-silv         ###   ########.fr       */
+/*   Updated: 2023/06/06 15:38:22 by sde-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,24 +26,23 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 	start = 0;
 	ptr = 0;
-	end = ft_strlen(s1);
+	end = ft_strlen(s1) - 1;
 	while (s1[start] && ft_strchr(set, s1[start]))
 		start ++;
 	while (s1[end] && ft_strchr(set, s1[end]))
 		end --;
-	ptr = malloc (sizeof(char) * (end - start + 1));
+	ptr = malloc (sizeof(char) * (end - start + 2));
 	if (ptr)
-		ft_strlcpy(ptr, s1 + start, (end - start));
+		ft_strlcpy(ptr, s1 + start, (end - start + 2));
 	return (ptr);
 }
-/*
+
 int	main(void)
 {
 	char *ptr;
 
-	ptr = ft_strtrim("  ", " ");
+	ptr = ft_strtrim("lorem ipsum dolor sit amet", "te");
 	write(1, ptr, ft_strlen(ptr));
 	free (ptr);
 	return (0);
 }
-*/
