da3293311b07ca6c690a40bc1da6b7be977a3c09
Shenya De Silva <shenya44@outlook.com>
2023-06-09 23:49:35 +0200

    Reworking on logic

--- ft_substr.c
+++ ft_substr.c
@@ -14,49 +14,12 @@
 #include "libft.h"
 /*
 #include <unistd.h>
-initiate pointer to NULL
-	if index/start is in s
-	go to index/start
-	check the size of the substring
-	compare it with len
-		if len - 1 < string after start
-			create memory area for size len
-			copy until len - 1
-			add null terminator
-		else
-			create memory area for size + 1
-			copy until size
-			add null terminator
-	return pointer
+
 */
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*ptr;
-	size_t	i;
-	size_t	sublen;
 
-	ptr = NULL;
-	i = 0;
-	if (s[i] && start <= ft_strlen(s))
-	{
-		while (s[i] && i < start)
-			i ++;
-	}
-	sublen = ft_strlen(s + start);
-	if (sublen < len)
-	{
-		ptr = malloc((sublen + 1) * sizeof(char));
-		if (ptr)
-			ft_strlcpy(ptr, (s + start), sublen + 1);
-	}
-	else if (sublen >= len)
-	{
-		ptr = malloc((len + 1) * sizeof(char));
-		if (ptr)
-			ft_strlcpy(ptr, (s + start), len + 1);
-	}
-	return (ptr);
 }
 /*
 int	main()
