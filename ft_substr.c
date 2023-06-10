267d6001fa429a48d004b7ac61861cec3b7c1887
Shenya De Silva <shenya44@outlook.com>
2023-06-10 17:44:25 +0200

    Changed the logic

--- ft_substr.c
+++ ft_substr.c
@@ -14,18 +14,45 @@
 #include "libft.h"
 /*
 #include <unistd.h>
+s: 		The string from which to create the substring.
+start: 	The start index of the substring in the string ’s’.
+len:	The maximum length of the substring.
 
+Return: The substring. OR NULL if the allocation fails.
+
+Desc: Allocates (with malloc(3)) and returns a substring from the string ’s’. 
+The substring begins at index ’start’ and is of maximum size ’len’.
+
+strlen(s) < start
+    ft_strdup("") : allocates sufficient memory for a copy of the string s1, does
+     the copy, and returns a pointer to it.
+strlen(s) >= start
+	strlen(s + start) < len (s is the limiting factor)
+		len = strlen(s + start)
+	strlen(s + start) => len (len is the limiting factor)
+Allocate mem (len + 1) of char size
 */
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char    *ft_substr(char const *s, unsigned int start, size_t len)
 {
+    char    *ptr;
 
+    if (!s)
+        return (0);
+    if (ft_strlen(s) < start)
+        return (ft_strdup(""));
+    if (ft_strlen(s + start) < len)
+        len = ft_strlen(s + start);
+    ptr = malloc(sizeof(char) * (len + 1));
+    if (!ptr)
+        return (0);
+    ft_strlcpy(ptr, s + start, len + 1);
+    return (ptr);
 }
 /*
-int	main()
+int    main(void)
 {
-	ft_putstr_fd(ft_substr("", 1, 1), 1);
-	write (1, "\n", 1);
-	return (0);
+    ft_substr("Hello World", 3, 5);
+    return (0);
 }
 */
