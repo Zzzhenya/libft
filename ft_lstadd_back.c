e9ba7fb14ee068592ec3cb58e73cac709712fcdc
Shenya De Silva <shenya44@outlook.com>
2023-10-01 14:43:31 +0200

    Fixed null node input scenario

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -4,11 +4,11 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*temp;
 
-	temp = *lst;
-	if (temp == 0)
-		temp = new;
+	if (*lst == 0)
+		*lst = new;
 	else
 	{
+		temp = *lst;
 		while (temp->next != 0)
 			temp = temp->next; 
 		temp->next = new;
